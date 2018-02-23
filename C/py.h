d3d869854645160c1d1a341f41fb88d5c98ebdfc
vipulbhj <vipulsworkmail@gmail.com>
2018-02-23 23:04:50 +0530

    Generic print added

---
+++ C/py.h
@@ -0,0 +1,22 @@
+#define and && 
+#define or ||
+#define printf_dec_format(x) _Generic((x), \
+    char: "%c", \
+    signed char: "%hhd", \
+    unsigned char: "%hhu", \
+    signed short: "%hd", \
+    unsigned short: "%hu", \
+    signed int: "%d", \
+    unsigned int: "%u", \
+    long int: "%ld", \
+    unsigned long int: "%lu", \
+    long long int: "%lld", \
+    unsigned long long int: "%llu", \
+    float: "%f", \
+    double: "%f", \
+    long double: "%Lf", \
+    char *: "%s", \
+    void *: "%p")
+ 
+#define print(x) printf(printf_dec_format(x), x)
+#define println(x) printf(printf_dec_format(x), x), printf("\n");
---
+++ C/test.c
@@ -0,0 +1,36 @@
+/*
+	#### Usage : --------------
+			@ gcc prog.c -std=c11
+
+*/
+
+#include<stdio.h>
+#include "py.h"
+
+int main(int argc, char *argv) {
+	/*
+		@ Test for and and or.
+		@ Test successful.
+		@ Tested for gcc compiler c11 version.
+	*/
+	if (1 == 1 and 1 == 2) 
+		printf("This should not work\n");
+	else if(1 == 1 or 1 == 2) 
+		printf("This should work\n");
+	else if (1 == 2) 
+		printf("This should work\n");
+
+	/*
+		@ Testing Generic Print Function.
+		@ Test successful.
+		@ Tested for gcc complier c11 version.
+	*/
+
+	println('a');    // prints "97" (on an ASCII system)
+    println((char)'a');  // prints "a"
+    println(123);    // prints "123"
+    println(1.234);      // prints "1.234000"
+	println("abcdefghi"); // prints "abcdefghi"
+
+	return 0;
+}
